2c66868dfd8f48bfb7e7bbf77f4b954ab52cecab
Duncan Law-Green <duncan.lawgreen@gmail.com>
2015-04-29 22:35:26 +0100

    Working tweepy Hello World

--- app/test_tweepy.py
+++ app/test_tweepy.py
@@ -19,6 +19,6 @@ auth.set_access_token(access_token, access_token_secret)
 
 api = tweepy.API(auth)
 
-public_tweets = api.home_timeline()
+public_tweets = api.user_timeline('Rocketeer_UK')
 for tweet in public_tweets:
     print tweet.text
